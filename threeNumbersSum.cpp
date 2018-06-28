a77f32a303a76c2450d947c5d802f329638af7d1
auniquesun <Mr.sunhy@outlook.com>
2018-06-28 10:45:52 +0800

    improve time efficency in the function threeSum

--- threeNumbersSum.cpp
+++ threeNumbersSum.cpp
@@ -29,56 +29,86 @@ using namespace std;
 
 
 vector< vector<int> > threeSum(vector<int>& nums) {
+    //clock_t start = clock();      // 测试时使用
+
+    //cout<<"start time: "<<float(start)/CLOCKS_PER_SEC<<endl;  
+
+    //cout<<"start time: "<<float(start)<<endl;
+
     vector< vector<int> > result;     // 定义一个最终要返回的结果，嵌套向量，相当于二维向量
+
     vector<int> triplet;    // 定义每次循环找到的三元组
 
     int n = nums.size();    // 获取nums数组长度
-    
+
     int i;
-    
+
     sort(nums.begin(), nums.end());     // 从小到大排序vector元素
-    
 
     for(i=0;i<n-1;i++){
-        
+
         int left = i+1;
+
         int right = n-1;
 
         for(;left<right;){
-            
-            if(nums[i] + nums[left] + nums[right] == 0){    // nums[right] >= nums[left], 和为零表明有正有负
-                triplet.push_back(nums[i]);
-                triplet.push_back(nums[left]);
-                triplet.push_back(nums[right]);
 
+          if(nums[i] + nums[left] + nums[right] == 0){    // nums[right] >= nums[left], 和为零表明有正有负
+
+                triplet.push_back(nums[i]);
+
+                triplet.push_back(nums[left]);
+
+                triplet.push_back(nums[right]);
                 result.push_back(triplet);
+
                 triplet.clear();
 
-                left++;
                 right--;    // 必须再往下寻找，看是否有其他满足条件的值，否则跳不出循环
+
                 //cout << opposite << " " << nums[i] << " " << nums[j] << endl;
+
             }
 
             else if(nums[i] + nums[left] + nums[right] < 0)
+
                 left++;
 
             else
+
                 right--;
-            
+
         }
-        
-            
-    }
+   }
 
-    result.erase( unique( result.begin(), result.end() ), result.end() );   // 移除 result 中的重复元素
 
-    for(int i=0;i<result.size();i++){
-        vector<int> row = result[i];
 
-        for(int j=0;j<row.size();j++)
-            cout << row[j] << " ";
-        cout<<endl;
-    }
+    //sort(result.begin(), result.end());
+
+    //result.erase( unique( result.begin(), result.end() ), result.end() );   // 移除 result 中的重复元素
+
+    // 移除 result 中的重复元素，实验结果证明，这种方法速度更快
+    set< vector<int> > s( result.begin(), result.end() );       
+    result.assign( s.begin(), s.end() );
+
+    //clock_t end = clock();            // 测试时使用
+
+    //cout<<"end time: "<<float(end)/CLOCKS_PER_SEC<<endl;
+
+    //cout<<"end time: "<<float(end)<<endl;
+
+    //cout<<"time cost is "<<float(end-start)/CLOCKS_PER_SEC*1000<<"ms"<<endl;      // 测试时使用
+
+//     for(int i=0;i<result.size();i++){          // 测试时使用
+
+//         vector<int> row = result[i];
+//         for(int j=0;j<row.size();j++)
+
+//             cout << row[j] << " ";
+
+//         cout<<endl;
+
+//     }
 
     return result;
 }
