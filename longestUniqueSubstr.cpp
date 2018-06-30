8bc85624c5431c68cec9d98e631e8c4879110ed2
auniquesun <Mr.sunhy@outlook.com>
2018-07-01 00:49:26 +0800

    fixed bugs in the function main

--- longestUniqueSubstr.cpp
+++ longestUniqueSubstr.cpp
@@ -11,106 +11,74 @@
 //     if cur_len > max_len
 //         max_len = cur_len
 
+// 不能默认输入的字符都是英文大小写，可以是各种各样的字符，
+// 以前的解法（默认输入只有英文大小写字符）存在严重错误
+
 #include <iostream>
 #include <string>
+#include <set>
 
 using namespace std;
 
-struct Node
-{
-	char ch;
-	int visited;
-};
-
-class charList{	//构建一张 (char) ==> (visited) 的映射表 	存放：大写字母A~Z,小写字母a~z；是否在substr出现过,用visited标记；认为大小写字母不一样
-public:
-    Node *node = NULL;
-    charList(){
-        node = new Node[52];
-        for(int i=0;i<52;i++){
-            if(i < 26){
-                node[i].ch = char(65+i);
-            }
-            else
-                node[i].ch = char(97 - 26 + i);
-
-            node[i].visited = 0;    //0 代表没有访问过
-        }
-    }
-    ~charList(){
-    	delete [] node;
-    }
-};
-
 int main() {
 
-    charList *cl = new charList();
     cout<<"===================================="<<endl;
     cout<<"input a string: ";
     string s;
     cin>>s;
 
+    set<char> char_set;
+
     int n = s.length();
+    if(n == 0)  //不可以默认最大长度为1，因为可能输入空字串
+        return 0;
+    else{
+        int i,j;
+        int max_len=1,cur_len=1;    
 
-    int i,j;
-    int max_len=1,cur_len=1;
+        int cur_start_pos = 0; //当前子串的起始位置
+        int cur_end_pos = 0;  //当前子串的结束位置
 
-    int cur_start_pos = 0; //当前子串的起始位置
-    int cur_end_pos = 0;  //当前子串的结束位置
+        char ch;    //存储当前字符
 
-    char ch;    //存储当前字符
-    int ch2idx; //把当前字符转换成charList中的索引
+        ch = s[0];
+        char_set.insert(ch);
 
-    ch = s[0];
-    ch2idx = int(ch);
+        for(i = 1;i < n;i++){
+            ch = s[i];
 
-    if(ch2idx >= 65 && ch2idx <= 90)
-        ch2idx -= 65;
-    else if(ch2idx >=97 && ch2idx <= 122)
-        ch2idx = ch2idx - 97 + 26;
+            if(char_set.find(ch) == char_set.end()){
+                cur_len++;
+                cur_end_pos++;
 
-    cl->node[ch2idx].visited = 1;
+                char_set.insert(ch);
+            }else{
+                int flag = 1;   //假设：当前子串内包含当前字符ch
+                for(j = cur_start_pos;j <= cur_end_pos;j++){
+                    if(s[j] == s[i]){   //①如果假设成立，进行处理
+                        cur_start_pos = j+1;    //重新定位新子串的开始位置, 出现过此字符的位置加1
+                        cur_end_pos = i;    //重新定位新子串的结束位置
+                        cur_len = cur_end_pos - cur_start_pos + 1;    //重新记录新子串的长度
+                        flag = 0;       //改变标志位
+                        j = cur_end_pos;    //退出for循环
+                    }
+                }
 
-    for(i = 1;i < n;i++){
-        ch = s[i];
-        ch2idx = int(ch);
-
-        if(ch2idx >= 65 && ch2idx <= 90)
-            ch2idx -= 65;
-        else if(ch2idx >=97 && ch2idx <= 122)
-            ch2idx = ch2idx - 97 + 26;
-
-        if(cl->node[ch2idx].visited == 0){  //情况一：前面的字符串没出现过这个字符
-            cur_len++;
-            cur_end_pos++;
-
-            cl->node[ch2idx].visited = 1;   //把当前扫描过的字符标记为visited
-        }else{   //情况二：前面的字符串出现过这个字符，还分为两种小情况
-            	   //小情况一：当前子字符串包含此字符
-
-            int flag = 1;   //假设：当前子串内包含当前字符ch
-            for(j = cur_start_pos;j <= cur_end_pos;j++){
-                if(s[j] == s[i]){   //①如果假设成立，进行处理
-                    cur_start_pos = j+1;    //重新定位新子串的开始位置, 出现过此字符的位置加1
-                    cur_end_pos = i;    //重新定位新子串的结束位置
-                    cur_len = cur_end_pos - cur_start_pos + 1;    //重新记录新子串的长度
-                    flag = 0;       //改变标志位
-                    j = cur_end_pos;    //退出for循环
+                if(flag){    //小情况二：当前子字符串不包含此字符
+                                //②如果假设不成立，标志位没有改变，flag == 1
+                    cur_len++;
+                    cur_end_pos++;
                 }
             }
 
-            if(flag){    //小情况二：当前子字符串不包含此字符
-            				//②如果假设不成立，标志位没有改变，flag == 1
-                cur_len++;
-                cur_end_pos++;
-            }
+            if(max_len < cur_len)
+                max_len = cur_len;            
+
         }
 
-        if(max_len < cur_len)
-            max_len = cur_len;            
+        cout << "the length of longest unique substr is: " << max_len <<endl;
+        cout<<"===================================="<<endl;
 
+        return max_len;
     }
-
-    cout << "the length of longest unique substr is: " << max_len <<endl;
-    cout<<"===================================="<<endl;
 }
