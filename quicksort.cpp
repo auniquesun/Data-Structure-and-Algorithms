1fddde8f324bdb7a64cb5f4c14dc2fb81a37e601
auniquesun <Mr.sunhy@outlook.com>
2018-12-10 16:40:53 +0800

    add idea

--- quicksort.cpp
+++ quicksort.cpp
@@ -7,6 +7,8 @@ using namespace std;
 /*
 reference：https://www.geeksforgeeks.org/quick-sort/
 
+idea:
+The key process in quickSort is partition(). Target of partitions is, given an array and an element x of array as pivot, put x at its correct position in sorted array and put all smaller elements (smaller than x) before x, and put all greater elements (greater than x) after x. All this should be done in linear time.
 */
 
 int partition(vector<int> &nums, int left, int right, int &iteration){
