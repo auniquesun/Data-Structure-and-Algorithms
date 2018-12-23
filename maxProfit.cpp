d197884c14bd26b0a20b98a6107f777c797c7301
auniquesun <Mr.sunhy@outlook.com>
2018-12-23 17:34:17 +0800

    compute the max profit of 'buy sell' stock in an array

--- maxProfit.cpp
+++ maxProfit.cpp
@@ -3,6 +3,19 @@
 
 using namespace std;
 
+/*
+problem:
+
+idea:
+这道题其实不太好想，其核心的思想是：
+要向赚取收益，一定要有buy、sell这一对操作，而且sell value > buy value才会有正收益
+    从左往右扫面数组，
+        寻找局部最小的点，即为buy点；
+        从buy点之后，继续寻找局部最大的点，即为sell点；
+        过程中要注意buy点最多扫面到数组倒数第二个元素；
+        如果数组递减，则没有正收益，收益最大为0
+*/
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
