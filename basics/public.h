df809f872b991adafe8573bda8d9fd6fc6c1893a
Sun Hongyu <Mr.sunhy@outlook.com>
2020-03-25 22:54:31 +0800

    add class TreeNode

--- basics/public.h
+++ basics/public.h
@@ -3,3 +3,12 @@
 #include<cstring> // strcpy头文件
 
 using namespace std;
+
+class TreeNode{
+    public:
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int v):val(v), left(NULL), right(NULL) {}
+};
+
