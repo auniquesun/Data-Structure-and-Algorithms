7b379093498a9302c312acab84e28e695cf117a6
Sun Hongyu <Mr.sunhy@outlook.com>
2020-05-25 17:00:58 +0800

    steps++ ==> ++steps

--- backtracing/maze.cpp
+++ backtracing/maze.cpp
@@ -96,7 +96,7 @@ int main()
     for(int i=0; i<10; i++){
         for(int j=0; j<16; j++)
             if(maze[i][j] == 'x')
-                steps++;
+                ++steps;
     }
     printf("least steps required from (1,1) to (8,13) are %d\n", steps-1);
     waitForEnter();
